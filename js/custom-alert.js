b19ea2d7e59d5df050f6c07cfb546a9f1d5d724e
Victor Chen <chenvictor1999@gmail.com>
2018-12-09 11:48:00 -0800

    Added a queue to CustomAlert to allow for multiple alerts to be shown in sequence.

--- js/custom-alert.js
+++ js/custom-alert.js
@@ -1,19 +1,52 @@
 const CustomAlert = new function() {
-    const modal = $('#alertModal');
-    const text = document.getElementById('alertModalMessage');
 
-    this.showMessage = function(message) {
-        // Fallback method to use if no modal found
-        console.debug("Displaying message: %s", message);
-        alert(message);
+    let fallback = function() {
+        function showMsg(msg) {
+            alert(msg);
+        }
+        return showMsg;
     };
-    if (modal === null || text === null) {
-        console.warn("Alert modal not found!");
-    } else {
-        this.showMessage = function (message) {
-            console.debug("Displaying message: %s", message);
-            text.innerHTML = message;
+
+    let modalAlert = (_modal, _text) => {
+        let modal = _modal;
+        let text = _text;
+        let isShowing = false;
+        let queue = [];
+        function showMsg(msg) {
+            if (isShowing) {
+                //Add message to queue
+                queue.push(msg);
+            } else {
+                showModal(msg);
+            }
+        }
+        function showModal(msg) {
+            isShowing = true;
+            text.innerHTML = msg;
             modal.modal('show');
-        };
-    }
+        }
+        function nextModal() {
+            if (queue.length === 0) {
+                isShowing = false;
+            } else {
+                showModal(queue.shift());
+            }
+        }
+        modal.on('hidden.bs.modal', nextModal);
+        return showMsg;
+    };
+
+    this.showMessage = () => {};    //Stub
+
+    window.addEventListener('load', () => {
+        let text = document.getElementById('alertModalMessage');
+        let modal = $('#alertModal');
+        if (modal === null || text === null) {
+            console.warn("Alert modal not found!");
+            this.showMessage = fallback();
+        } else {
+            this.showMessage = modalAlert(modal, text);
+        }
+    });
+
 };
